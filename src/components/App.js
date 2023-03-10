dd08f37cdeb2c4f4c51f1ade72b6c9f6b2ddfe36
Josh Warburton <joshua.warburton@zuto.com>
2023-03-10 08:47:08 +0000

    Added Header

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./home/Home";
 import About from "./about/About";
+import Header from "./common/Header"
 
 function App() {
   return (
     <div className="container-fluid">
+      <Header/>
       <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
+      <Route path="/about" component={About} />
     </div>
   );
 }
